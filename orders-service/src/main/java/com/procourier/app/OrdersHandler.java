f87503dcb01174553f1f0007f000b4d43fc4da26
taralesca <ovidiu.taralesca@gmail.com>
2019-03-21 20:04:41 +0200

    Server running

---
+++ orders-service/src/main/java/com/procourier/app/OrdersHandler.java
@@ -0,0 +1,19 @@
+package com.procourier.app;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class OrdersHandler implements Route {
+
+    private OrdersHandler() {
+
+    }
+
+    public static final OrdersHandler INSTANCE = new OrdersHandler();
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return "Hello World";
+    }
+}
